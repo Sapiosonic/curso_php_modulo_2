1264a619ade4a7dc94ddd9185cdda5f9a3b50357
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-06-19 23:08:30 -0300

    Loading internal pages

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php require "../app/functions/pages.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP para quem sabe PHP</title>
+    <link 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"
+    >
+  </head>
+  <body>
+    <div class="container">
+      <?php require load(); ?>
+    </div>
+  </body>
+</html>
