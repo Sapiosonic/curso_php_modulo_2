71e9fd89f6e1f4cd6941aa37cab628deea6c8197
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-07-13 21:53:47 -0300

    Working on the form page

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -1,1 +1,28 @@
 <h2>Contato</h2>
+
+<form action="/pages/forms/contato.php" method="post" role="form">
+  <legend>Entre em contato</legend>
+
+  <div class="form-group">
+    <label for="">Nome</label>
+    <input type="text" name="" name="name" class="form-control" placeholder="Digite seu nome">
+  </div>
+
+  <div class="form-group">
+    <label for="">E-mail</label>
+    <input type="text" name="" name="email" class="form-control" placeholder="IDigite seu e-mail">
+  </div>
+
+  <div class="form-group">
+    <label for="">Assunto</label>
+    <input type="text" name="" name="subject" class="form-control" placeholder="Digite o assunto">
+  </div>
+
+  <div class="form-group">
+    <label for="">Mensagem</label>
+    <textarea name="message" cols="30" rows="10" class="form-control" placeholder="Digite aqui sua mensagem"></textarea>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
