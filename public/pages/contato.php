50178a148d9843e922df671369356482a72ee527
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-07-13 22:33:25 -0300

    Fixing name attribute duplicate on form fields

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -5,17 +5,17 @@
 
   <div class="form-group">
     <label for="">Nome</label>
-    <input type="text" name="" name="name" class="form-control" placeholder="Digite seu nome">
+    <input type="text" name="name" class="form-control" placeholder="Digite seu nome">
   </div>
 
   <div class="form-group">
     <label for="">E-mail</label>
-    <input type="text" name="" name="email" class="form-control" placeholder="IDigite seu e-mail">
+    <input type="text" name="email" class="form-control" placeholder="Digite seu e-mail">
   </div>
 
   <div class="form-group">
     <label for="">Assunto</label>
-    <input type="text" name="" name="subject" class="form-control" placeholder="Digite o assunto">
+    <input type="text" name="subject" class="form-control" placeholder="Digite o assunto">
   </div>
 
   <div class="form-group">
