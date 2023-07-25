ce95608e2394df78dbcc2ac89ebc7bc449f8b248
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-07-25 18:17:38 -0300

    "Implementando requisição do tipo POST"

--- app/functions/custom.php
+++ app/functions/custom.php
@@ -5,3 +5,10 @@ function dd($dump){
   var_dump($dump);
   die();
 }
+
+function request(){
+  $request = $_SERVER['REQUEST_METHOD'];
+  $check_method = ($request == 'POST') ?  $_POST :  $_GET;
+
+  return $check_method;
+}
